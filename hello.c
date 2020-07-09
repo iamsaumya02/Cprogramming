25f853229dfa888c85a26f117e841fdb3db1cead
iamsaumya02 <summasrivastava@gmail.com>
2020-07-09 15:35:44 +0530

    arithmetic operators

--- hello.c
+++ hello.c
@@ -1,2 +1,16 @@
-i
-
+    #include<stdio.h>
+   int main()
+{
+	int a = 9, b = 4, c;
+	c = a+b;
+	printf("a+b = %d \n",c);
+	c = a-b;
+	printf("a-b = %d \n",c);
+	c = a*b;
+	printf("a*b = %d \n",c);
+	c = a/b;
+	printf("a/b = %d \n",c);
+	c = a%b;
+	printf("Remainder when a divided by b = %d \n",c);
+	return 0;
+}c
